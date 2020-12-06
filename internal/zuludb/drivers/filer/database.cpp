f7eec6ee9afa6d282d4bcd5722e260b89dd38f7d
apostolis anastasiou <apostolis.anastasiou.alpha@gmail.com>
2020-12-06 11:25:09 +0200

    WIP: Add getRecordByID and getIndexByRecordID

--- internal/zuludb/drivers/filer/database.cpp
+++ internal/zuludb/drivers/filer/database.cpp
@@ -1,67 +1,103 @@
-#include <iostream>
-#include <fstream>
-#include <cstring>
-
 #include "./database.hpp"
 
-Database::Database(const char* filename) {
-	this->_filename = (char*) malloc(strlen(filename));
-	std::strcpy(this->_filename, filename);
-	this->_stream = std::fstream(
-			getFilename(),
-			std::fstream::app
-			| std::fstream::in
-			| std::fstream::out
-			| std::fstream::binary
-	);
-	this->_indexStream = std::fstream(
-			this->getIndexFilename(),
-			std::fstream::app
-			| std::fstream::in
-			| std::fstream::out
-	);
+#include <cstring>
+#include <fstream>
+#include <iostream>
+
+Database::Database (const char* filename)
+{
+    this->_filename = (char*) malloc (strlen (filename));
+    std::strcpy (this->_filename, filename);
+    this->_stream
+	= std::fstream (getFilename (),
+			std::fstream::app | std::fstream::in | std::fstream::out
+			    | std::fstream::binary);
+    this->_indexStream
+	= std::fstream (this->getIndexFilename (),
+			std::fstream::app | std::fstream::in | std::fstream::out
+			    | std::fstream::binary);
 }
 
-Database::~Database() {
-	this->_stream.close();
-	this->_indexStream.close();
-	free(this->_filename);
+Database::~Database ()
+{
+    this->_stream.close ();
+    this->_indexStream.close ();
+    free (this->_filename);
 }
 
-bool Database::isOpen() {
-	return this->_stream.is_open();
+bool
+    Database::isOpen ()
+{
+    return this->_stream.is_open ();
 }
 
-void Database::close() {
-	this->_stream.close();
+void
+    Database::close ()
+{
+    this->_stream.close ();
 }
 
-void Database::write(Record record) {
-	this->_stream.write(
-			reinterpret_cast<char*>(record.getData()),
-			sizeof(record.getData())
-	);
+void
+    Database::write (Record record)
+{
+    std::string data;
+    data.append (reinterpret_cast<char*> (record.getID ()));
+    data.append (reinterpret_cast<char*> (record.getData ()));
+    this->_stream.write (data.c_str (), sizeof (record.getData ()));
+    // Index record
+    ZL_DatabaseIndexArgs indexArgs = {};
+    indexArgs.index = this->_stream.tellg ();
+    indexArgs.record = record;
+    this->index (indexArgs);
 }
 
-void Database::index(Record record, uint32_t index) {
-	// Allocate 2 * uint32_t
-	char* indexStr = (char*) malloc(sizeof(char) * 2);
-	// Add ID
-	std::strcpy(indexStr, reinterpret_cast<char*>(record.getID()));
-	// Add index
-	std::strcpy(indexStr, reinterpret_cast<char*>(record.getID()));
-	// Write to file
-	this->_indexStream.write(
-		indexStr,
-		sizeof(uint32_t)
-	);
+void
+    Database::index (ZL_DatabaseIndexArgs args)
+{
+    // Allocate 2 * uint32_t
+    char* indexStr = (char*) malloc (sizeof (uint32_t) * 2);
+    // Add ID
+    std::strcpy (indexStr, reinterpret_cast<char*> (args.record.getID ()));
+    // Add index
+    std::strcpy (indexStr, reinterpret_cast<char*> (args.index));
+    // Write to file
+    this->_indexStream.write (indexStr, sizeof (char) * std::strlen (indexStr));
 }
 
-std::string Database::getFilename() {
-	return this->_filename;
+Record
+    Database::getRecordByID (uint32_t id)
+{
+    // Seek position (avoid ID which we know)
+    auto recordIndexData = this->getIndexByRecordID (id);
+    this->_stream.seekg (recordIndexData.index + sizeof (id));
+
+    // Extract record
+    char* recordData;
+    this->_stream.read (recordData, recordIndexData.size);
+
+    // Reconstruct record
+    ZL_RecordStruct rStruct = {};
+    rStruct.data = recordData;
+    rStruct.id = id;
+    Record r (rStruct);
+
+    return r;
 }
-std::string Database::getIndexFilename() {
-	std::string indexFilename(this->_filename);
-	indexFilename.append(".index");
-	return indexFilename;
+
+ZL_DatabaseIndex
+    Database::getIndexByRecordID (uint32_t id)
+{
+}
+
+std::string
+    Database::getFilename ()
+{
+    return this->_filename;
+}
+std::string
+    Database::getIndexFilename ()
+{
+    std::string indexFilename (this->_filename);
+    indexFilename.append (".index");
+    return indexFilename;
 }
--- internal/zuludb/drivers/filer/database.hpp
+++ internal/zuludb/drivers/filer/database.hpp
@@ -1,26 +1,40 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 #include "./record.hpp"
+
+typedef struct ZL_DatabaseIndexArgs {
+    Record record;
+    uint32_t index;
+} ZL_DatabaseIndexArgs;
+
+typedef struct ZL_DatabaseIndex {
+    uint32_t index;
+    uint32_t id;
+    uint32_t size;
+} ZL_DatabaseIndex;
 
 /*
  * Database stores a databases meta information
  */
 class Database {
-	public:
-		Database(const char* filename);
-		~Database();
-		bool isOpen();
-		void close();
-		void write(Record record);
-		std::string getIndexFilename();
-		std::string getFilename();
-	private:
-		std::fstream _stream;
-		std::fstream _indexStream;
-		char* _filename;
+   public:
+    Database (const char* filename);
+    ~Database ();
+    bool isOpen ();
+    void close ();
+    void write (Record record);
+    Record getRecordByID (uint32_t id);
+    ZL_DatabaseIndex getIndexByRecordID (uint32_t id);
+    std::string getIndexFilename ();
+    std::string getFilename ();
 
-		void index(Record record, uint32_t index);
+   private:
+    std::fstream _stream;
+    std::fstream _indexStream;
+    char* _filename;
+
+    void index (ZL_DatabaseIndexArgs);
 };
 
